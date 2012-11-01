c34a453ba9b6e0e6773e0e15f6d474002f44a44a
Evan Carmi <evan@ecarmi.org>
2012-10-31 20:23:15 -0400

    Updated to fall 12

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-coffee -o wespoints -c calc.coffee
-haml index.haml index.html
-sass style.sass style.css
+coffee -o compiled -c calc.coffee
+haml index.haml compiled/index.html
+sass style.sass compiled/style.css
+echo "Done - compiled to compiled/"
